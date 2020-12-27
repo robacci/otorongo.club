0227637907d5f5661f49b0d7c38606941d060375
AniversarioPeru <aniversarioperu1@gmail.com>
2020-12-26 23:25:03 -0500

    static

--- otorongo/settings/production.py
+++ otorongo/settings/production.py
@@ -5,3 +5,8 @@ DATABASES["default"]["USER"] = "postgres"
 DATABASES["default"]["HOST"] = "db"
 
 ALLOWED_HOSTS = ['localhost', 'otorongo.club']
+
+MEDIA_ROOT = "/data/media/"
+STATIC_ROOT = "/data/static/"
+
+DEBUG = False
