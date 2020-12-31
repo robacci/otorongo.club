355b828c99c2a7e6c015d7606d86efe9b4b41e5f
AniversarioPeru <aniversarioperu1@gmail.com>
2020-12-31 18:45:24 -0500

    cron

--- docker/cron/setup_tweeting_candidates.sh
+++ docker/cron/setup_tweeting_candidates.sh
@@ -1,4 +1,4 @@
 if ! crontab -l | grep -q 'send_tweet'; then
   echo 'entry does not exist'
-  (crontab -l 2>/dev/null; echo "20,40,59 13-3/1 * * * /code/python manage.py send_tweet") | crontab -
+  (crontab -l 2>/dev/null; echo "20,40,59 13-3/1 * * * /usr/local/bin/python /code/manage.py send_tweet") | crontab -
 fi
