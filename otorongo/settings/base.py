1acb532a380088ba6f9de70996c0e89333fad16c
AniversarioPeru <aniversarioperu1@gmail.com>
2020-12-31 10:28:19 -0500

    use env

--- otorongo/settings/base.py
+++ otorongo/settings/base.py
@@ -9,18 +9,22 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 BASE_DIR = Path(__file__).resolve().parent.parent
 APPS_DIR = BASE_DIR / 'otorongo'
+
+env = environ.Env()
+env.read_env(f'{ROOT_DIR}/.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=z=ezb)3wnpm9wo_(4*itm!zq@xrvrzi#^z0#nz-sx^dh-4o5a'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
